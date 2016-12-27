f81836378dda1e4d043b622dc2f5974ea72634f7
jiuguiwhisky <jiuguiwhisky@gmail.com>
2016-12-27 18:34:45 +1000

    Update Reverse complement.py

--- Reverse complement.py
+++ Reverse complement.py
@@ -1,7 +1,8 @@
 # 第一次用biophython！
 
-def rev(Text): 
-
-    from Bio.Seq import Seq
-    reverse = Seq(Text) 
-    print (reverse.reverse_complement())
+data = open("c:\sample.txt")# 数据量太大不适合直接拷贝到python中，读取txt文件效率高很多
+seq=data.read()# 读取数据后格式并不是string,但Seq函数需要读取string格式
+seq=str(seq)# string化
+from Bio.Seq import Seq# Biopython的包
+reverse = Seq(seq) 
+print (reverse.reverse_complement())#reverse_complement的函数将配对碱基，并从3'到5'重新排列
